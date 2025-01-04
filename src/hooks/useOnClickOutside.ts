4137c3384bfab7c0cbce1f6102f4469aea54836f
Oslo <Oslo.noh@bunjang.co.kr>
2025-01-04 17:17:40 +0900

    useOnClickOutsideTest code apply

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -4,18 +4,18 @@ type Handler = (event: MouseEvent | TouchEvent) => void;
 
 function useOnClickOutside<T extends HTMLElement = HTMLElement>(
   ref: RefObject<T>,
-  handler: Handler
+  callback: Handler
 ): void {
   useEffect(() => {
     const listener = (event: MouseEvent | TouchEvent) => {
       const element = ref?.current;
+      if (!ref.current) return;
       
-      // ref가 없거나 클릭이 ref 엘리먼트 내부에서 발생한 경우
       if (!element || element.contains((event.target as Node))) {
         return;
       }
 
-      handler(event);
+      callback(event);
     };
 
     document.addEventListener('mousedown', listener);
@@ -25,7 +25,7 @@ function useOnClickOutside<T extends HTMLElement = HTMLElement>(
       document.removeEventListener('mousedown', listener);
       document.removeEventListener('touchstart', listener);
     };
-  }, [ref, handler]);
+  }, [ref, callback]);
 }
 
 export default useOnClickOutside;
